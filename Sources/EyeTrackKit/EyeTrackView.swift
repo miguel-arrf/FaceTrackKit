9cfb7d1a71bd0efcc9e84bd916c87e599305086e
Miguel Ferreira <mferreira.oeiras@gmail.com>
2023-04-18 01:52:39 +0100

    Adds eyetrack view functions back

--- Sources/EyeTrackKit/EyeTrackView.swift
+++ Sources/EyeTrackKit/EyeTrackView.swift
@@ -33,6 +33,8 @@ public struct EyeTrackView: UIViewRepresentable {
 
 
     public func makeUIView(context: Context) -> ARSCNView {
+        self.sceneView.delegate = context.coordinator
+        self.sceneView.session.delegate = context.coordinator
         self.sceneView.isHidden = self.isHidden
         self.sceneView.automaticallyUpdatesLighting = true
 
@@ -68,14 +70,65 @@ public struct EyeTrackView: UIViewRepresentable {
 
     /// Stop to record and Save the recorded video to Photo Library
     public func stopRecord(finished: @escaping (URL) -> Void = { _ in }, isExport: Bool = false) {
-        if isExport {
-        } else {
-
-        }
+      
     }
 
     public func makeCoordinator() -> Coordinator {
-        return Coordinator()
+        return Coordinator(view: $sceneView, eyeTrack: self.eyeTrack)
     }
 
+    public class Coordinator: NSObject, ARSCNViewDelegate, ARSessionDelegate {
+        @Binding public var view: ARSCNView
+        public var eyeTrack: EyeTrack
+
+        public init (view: Binding<ARSCNView>, eyeTrack: EyeTrack) {
+            self._view = view
+            self.eyeTrack = eyeTrack
+            super.init()
+            // Register EyeTrack module
+            self.eyeTrack.registerSceneView(sceneView: self.view)
+        }
+
+        deinit {
+            // Pause recording
+            // Pause the view's session
+            self.view.session.pause()
+        }
+
+        public func frame(didRender buffer: CVPixelBuffer, with time: CMTime, using rawBuffer: CVPixelBuffer) {
+            DispatchQueue.main.async {
+                self.eyeTrack.updateFrame(pixelBuffer: rawBuffer)
+            }
+        }
+
+        public func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+            self.eyeTrack.face.node.transform = node.transform
+            guard let faceAnchor = anchor as? ARFaceAnchor else {
+                return
+            }
+            updateAnchor(withFaceAnchor: faceAnchor)
+        }
+
+        public func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+            guard let sceneTransformInfo = view.pointOfView?.transform else {
+                return
+            }
+            // Update Virtual Device position
+            self.eyeTrack.device.node.transform = sceneTransformInfo
+        }
+
+        public func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+            self.eyeTrack.face.node.transform = node.transform
+            guard let faceAnchor = anchor as? ARFaceAnchor else {
+                return
+            }
+            updateAnchor(withFaceAnchor: faceAnchor)
+        }
+
+        public func updateAnchor(withFaceAnchor anchor: ARFaceAnchor) {
+            DispatchQueue.main.async {
+                self.eyeTrack.update(anchor: anchor)
+            }
+        }
+    }
 }
